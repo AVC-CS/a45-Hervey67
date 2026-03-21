d96ff36962d33f2ae067118047235cc22c1e45e2
Hervey67 <herv2007@gmail.com>
2026-03-20 19:31:06 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -13,5 +13,45 @@ int main()
 
     // TODO: Find min, max, and med among num1, num2, num3
 
+    if (num1 < num2){
+        if (num1 < num3){
+            if (num3 < num2){
+                min = num1;
+                max = num2;
+                med = num3;
+            }
+            else{
+                min = num1;
+                max = num3;
+                med = num2;
+            }
+        }
+        else{
+            min = num3;
+            max = num2;
+            med = num1; 
+        }
+
+    }
+    else{
+        if (num2 < num3){
+            if (num1 < num3){
+                min = num2;
+                max = num3;
+                med = num1; 
+            }
+            else{
+                min = num2;
+                max = num1; 
+                med = num3; 
+            }
+        }
+        else{
+            min = num3;
+            max = num1;
+            med = num2; 
+        }
+    }
+
     cout << min << " " << med << " " << max << endl;
 }
